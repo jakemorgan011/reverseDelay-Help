cd34ab8b5d741e6adaa24c6c1c5a97bb54daae2c
jakemorgan011 <160269977+jakemorgan011@users.noreply.github.com>
2024-06-21 12:36:44 -0700

    source code upload

---
+++ source/lookAndFeel.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    lookAndFeel.h
+    Created: 26 Apr 2024 11:21:59am
+    Author:  Jake Morgan
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "JuceHeader.h"
+
+class customLookAndFeel : public juce::LookAndFeel_V4{
+public:
+    
+    void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos, float rotaryStartAngle, float rotaryEndAngle, juce::Slider& slider){
+        
+        auto sprite = juce::ImageCache::getFromMemory(BinaryData::LeKnob_png, BinaryData::LeKnob_pngSize);
+        
+        auto image_width = sprite.getWidth();
+        auto num_slides = sprite.getHeight() / image_width;
+        auto target_slide = num_slides * sliderPos;
+        float sliderHeight = 80;
+        float sliderWidth = 80;
+        
+        auto start_y = target_slide * image_width;
+        
+        g.drawImage(sprite, 0, 0, sliderWidth, 80, 0, start_y, image_width, sliderHeight);
+    }
+    
+};
